600f2d85240cd1180f70cf361d542f6bb863ef5b
Rafael Pavan <rafael.202010386@unilasalle.edu.br>
2022-11-02 12:34:54 -0300

    pastas projeto

---
+++ src/main/java/com/nt/locadorant/LocadoraNtApplication.java
@@ -0,0 +1,13 @@
+package com.nt.locadorant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraNtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraNtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nt/locadorant/LocadoraNtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.locadorant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraNtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
