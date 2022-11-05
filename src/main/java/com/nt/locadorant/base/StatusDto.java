da8221cef6a901913480052bf36cc3b7370be520
Rafael Pavan <rafael.202010386@unilasalle.edu.br>
2022-11-05 11:02:00 -0300

    adicionando base e builder

---
+++ src/main/java/com/nt/locadorant/base/StatusDto.java
@@ -0,0 +1,28 @@
+package com.nt.locadorant.base;
+
+public class StatusDto {
+
+    private Integer code;
+    private String name;
+
+    public StatusDto(Integer code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
